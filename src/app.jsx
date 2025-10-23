adf24882ef630f5df8ad4f4c4c16edf9db110c3e
Grant <granttc@byu.edu>
2025-10-22 19:37:57 -0600

    started implementing login function

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
@@ -7,35 +8,45 @@ import { Login } from './login/login';
 import { Join } from './join/join';
 import { Tracker } from './tracker/tracker';
 import { About } from './about/about';
-
+import {AuthState } from './login/authState'
 
 export default function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
   return (
-    <BrowserRouter>
+  <BrowserRouter>
       <div className="body bg-dark text-light">
         <header className="container-fluid">
           <nav className="navbar fixed-top navbar-dark">
-            <div className="navbar-brand">
-              Table Top Tracker<sup></sup>
-            </div>
+            <div className="navbar-brand">Table Top Tracker</div>
             <menu className="navbar-nav">
+              {/* Login link always visible */}
               <li className="nav-item">
                 <NavLink className="nav-link" to="/">
                   Login
                 </NavLink>
               </li>
+
+              {/* Conditional links based on auth */}
+              {authState === AuthState.Authenticated && (
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/join">
+                      Join
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/tracker">
+                      Scores
+                    </NavLink>
+                  </li>
+                </>
+              )}
+
               <li className="nav-item">
-                <NavLink className="nav-link" to="join">
-                  Join
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="tracker">
-                  Scores
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="about">
+                <NavLink className="nav-link" to="/about">
                   About
                 </NavLink>
               </li>
@@ -44,27 +55,46 @@ export default function App() {
         </header>
 
         <Routes>
-            <Route path='/' element={<Login />} exact />
-            <Route path='/join' element={<Join />} />
-            <Route path='/tracker' element={<Tracker />} />
-            <Route path='/about' element={<About />} />
-            <Route path='*' element={<NotFound />} />
+          <Route
+            path="/"
+            element={
+              <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(newUserName, newAuthState) => {
+                  setUserName(newUserName);
+                  setAuthState(newAuthState);
+                  localStorage.setItem('userName', newUserName);
+                }}
+              />
+            }
+          />
+          <Route path="/join" element={<Join />} />
+          <Route path="/tracker" element={<Tracker />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
 
         <footer className="bg-dark text-white-50">
           <div className="container-fluid">
             <span className="text-reset">Grant Chamberlain</span>
-            <a className="text-reset" href="https://github.com/Grant-Chamberlain/Fall2025Startup">
+            <a
+              className="text-reset"
+              href="https://github.com/Grant-Chamberlain/Fall2025Startup"
+            >
               Source
             </a>
           </div>
         </footer>
       </div>
-  </BrowserRouter>
+    </BrowserRouter>
   );
-   
-  
-  function NotFound() {
-  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
+
+function NotFound() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      404: Return to sender. Address unknown.
+    </main>
+  );
 }
